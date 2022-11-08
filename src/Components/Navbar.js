c29dfb8d203503b2720016d6329fdd0c26f204c3
Divyansi Mishra <divyansim01@gmail.com>
2022-11-08 11:08:39 +0530

    new

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = (props) => {
         <div>
             <nav className="navbar fixed-top navbar-expand-lg bg-dark navbar-dark">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">NewsUpdates</Link>
+                    <Link className="navbar-brand" to="/General">NewsUpdates</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
