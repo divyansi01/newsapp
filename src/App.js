bcd79ea7d419dccc9374efdcc827ddb30c458552
Divyansi Mishra <divyansim01@gmail.com>
2022-11-08 11:34:39 +0530

    new

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import News from './Components/News';
 import {
   BrowserRouter as Router,
   Routes,
-  Route, HashRouter,
+  Route,
 } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
@@ -21,24 +21,21 @@ const App = () => {
   return (
     <div>
       <Router>
-        <HashRouter basename="/">
-          <Navbar />
-          <LoadingBar
-            color='#f11946'
-            progress={progress}
-          />
-          <News setProgres={setProgress} key="General" pageSize={pageSize} country="in" category="General" />
-          <Routes>
-            <Route exact path="/" element={<News setProgres={setProgress} key="General" pageSize={pageSize} country="in" category="General" />}></Route>
-            <Route exact path="/Business" element={<News setProgres={setProgress} key="Business" pageSize={pageSize} country="in" category="Business" />}></Route>
-            <Route exact path="/General" element={<News setProgres={setProgress} key="General" pageSize={pageSize} country="in" category="General" />}></Route>
-            <Route exact path="/Science" element={<News setProgres={setProgress} key="Science" pageSize={pageSize} country="in" category="Science" />}></Route>
-            <Route exact path="/Technology" element={<News setProgres={setProgress} key="technology" pageSize={pageSize} country="in" category="Technology" />}></Route>
-            <Route exact path="/Sports" element={<News setProgres={setProgress} key="Sports" pageSize={pageSize} country="in" category="Sports" />}></Route>
-            <Route exact path="/Entertainment" element={<News setProgres={setProgress} key="Entertainment" pageSize={pageSize} country="in" category="Entertainment" />}></Route>
-            <Route exact path="/Health" element={<News setProgres={setProgress} key="Health" pageSize={pageSize} country="in" category="Health" />}></Route>
-          </Routes>
-        </HashRouter>
+        <Navbar />
+        <LoadingBar
+          color='#f11946'
+          progress={progress}
+        />
+        <Routes>
+          <Route exact path="/" element={<News setProgres={setProgress} key="General" pageSize={pageSize} country="in" category="General" />}></Route>
+          <Route exact path="/Business" element={<News setProgres={setProgress} key="Business" pageSize={pageSize} country="in" category="Business" />}></Route>
+          <Route exact path="/General" element={<News setProgres={setProgress} key="General" pageSize={pageSize} country="in" category="General" />}></Route>
+          <Route exact path="/Science" element={<News setProgres={setProgress} key="Science" pageSize={pageSize} country="in" category="Science" />}></Route>
+          <Route exact path="/Technology" element={<News setProgres={setProgress} key="technology" pageSize={pageSize} country="in" category="Technology" />}></Route>
+          <Route exact path="/Sports" element={<News setProgres={setProgress} key="Sports" pageSize={pageSize} country="in" category="Sports" />}></Route>
+          <Route exact path="/Entertainment" element={<News setProgres={setProgress} key="Entertainment" pageSize={pageSize} country="in" category="Entertainment" />}></Route>
+          <Route exact path="/Health" element={<News setProgres={setProgress} key="Health" pageSize={pageSize} country="in" category="Health" />}></Route>
+        </Routes>
       </Router>
     </div>
   )
